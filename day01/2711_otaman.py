2de0290cebd570200b386d6c3cdf64858e8d242a
kdh <kdh462601@gmail.com>
2024-11-07 22:08:24 +0900

    2711_otaman 문제풀이

--- day01/2711_otaman.py
+++ day01/2711_otaman.py
@@ -1,6 +1,17 @@
+# T = int(input())
+#
+# for _ in range(T):
+#     idx, word = input().split()
+#     idx = int(idx) -1
+#     modified_word = word[:idx] + word[idx + 1:]
+#     print(modified_word);
+
 T = int(input())
 
 for _ in range(T):
     idx, word = input().split()
-    idx = int(idx)
-
+    idx = int(input()) -1
+    word = list(word)
+    word.pop(idx)
+    ans = ''.join(word)
+print(ans)
